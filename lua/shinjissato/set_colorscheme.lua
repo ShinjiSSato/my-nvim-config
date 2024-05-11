fbad4597c3658a1f72874c34dd5315c234a3047a
ShinjiSSato <shinjisato69@proton.me>
2024-05-11 12:32:43 -0300

    UPDATE: Change colorscheme to catppuccin.

--- lua/shinjissato/set_colorscheme.lua
+++ lua/shinjissato/set_colorscheme.lua
@@ -1,3 +1,10 @@
-vim.o.background = "light" -- or "dark" for dark mode
-vim.cmd([[colorscheme gruvbox]])
+-- Gruvbox colorscheme
+
+-- vim.o.background = "light" -- or "dark" for dark mode
+-- vim.cmd([[colorscheme gruvbox]])
+
+-- Tokyonight colorscheme
 -- vim.cmd([[colorscheme tokyonight-storm]])
+
+-- Catpuccin colorscheme
+vim.cmd.colorscheme "catppuccin"
